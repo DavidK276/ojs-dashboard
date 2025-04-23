2b1488af0a9528cb3ff8129cb02048649f429f95
David Krchňavý <david.krchnavy@gmail.com>
2025-04-23 09:51:13 +0200

    redirect to participants from home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
+
+	onMount(() => {
+		goto('/participants');
+	});
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
